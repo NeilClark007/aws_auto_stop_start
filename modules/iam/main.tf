dc0e69940fbf74783225b5523a1e31dd943d8184
NeilClark007 <88084741+NeilClarkAtos@users.noreply.github.com>
2023-07-04 16:51:38 +0100

    update

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -27,5 +27,4 @@ resource "aws_iam_role_policy_attachment" "ssm_automation_attachment" {
   role       = aws_iam_role.ssm_role_automation.name
   # if you are using custom policies rather than AWS policies you will need to update this policy arn with your policy arn
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonSSMAutomationRole"
-
 }
